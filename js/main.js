18c44af447d63c50ae8e06ff9842a43c3f806532
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-06 15:57:48 +0800

    Initialize holidays data on page load with default serviceType

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 // js/main.js
+
 import { populateCountries } from './ui/countryUtils.js'; // Import country population and holiday fetching functions
 import { setupEventListeners } from './eventHandlers/eventHandlers.js'; // Import the event handlers
 import { setupSwitchButton } from './switch/switch.js'; // Import the switch button setup function
 import { initializeTimezone } from './timezone/timezone.js'; // Import the timezone initialization function
 
 document.addEventListener('DOMContentLoaded', () => {
-    populateCountries(); // Populate countries dropdown and fetch holidays
+    const serviceTypeElement = document.getElementById('serviceType');
+    const defaultServiceType = serviceTypeElement ? serviceTypeElement.value : 'default'; // Use 'default' if serviceType is not set
+    
+    populateCountries(defaultServiceType); // Populate countries dropdown and fetch holidays based on default serviceType
     setupEventListeners(); // Setup event listeners for UI elements
     setupSwitchButton(); // Initialize the switch button functionality
     initializeTimezone(); // Initialize the timezone functionality
