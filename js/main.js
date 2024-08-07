067e307d2a27b44b4d4b867c1cf7d5759c4750bd
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-07 08:36:04 +0800

    Add import for customDropdown.js in main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-
 // js/main.js
 
 import { populateCountries } from './ui/countryUtils.js'; // Import country population and holiday fetching functions
 import { setupEventListeners } from './eventHandlers/eventHandlers.js'; // Import the event handlers
 import { setupSwitchButton } from './switch/switch.js'; // Import the switch button setup function
 import { initializeTimezone } from './timezone/timezone.js'; // Import the timezone initialization function
+import './customDropdown/customDropdown.js'; // Import custom dropdown functionality
 
 document.addEventListener('DOMContentLoaded', () => {
     const serviceTypeElement = document.getElementById('serviceType');
