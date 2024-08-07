118417d5e6fdbc989bffe6a8356e64777b7bf8b3
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-07 08:12:44 +0800

    reverted back

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // js/main.js
 
 import { populateCountries } from './ui/countryUtils.js'; // Import country population and holiday fetching functions
@@ -8,16 +9,9 @@ import { initializeTimezone } from './timezone/timezone.js'; // Import the timez
 document.addEventListener('DOMContentLoaded', () => {
     const serviceTypeElement = document.getElementById('serviceType');
     const defaultServiceType = serviceTypeElement ? serviceTypeElement.value : 'default'; // Use 'default' if serviceType is not set
-
+    
     populateCountries(defaultServiceType); // Populate countries dropdown and fetch holidays based on default serviceType
     setupEventListeners(); // Setup event listeners for UI elements
     setupSwitchButton(); // Initialize the switch button functionality
     initializeTimezone(); // Initialize the timezone functionality
-
-    // Initialize Select2 for the country select element
-    $('#countrySelect').select2({
-        theme: 'default', // Keeps the default styling which can be customized
-        placeholder: 'Select a country',
-        width: 'resolve' // Ensure it fits the container
-    });
 });
