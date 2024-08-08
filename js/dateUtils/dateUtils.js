de49f8b49690fbbfe339018a77f3d21908ba0ba8
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-09 00:51:15 +0800

    DONT TOUCH THIS

--- js/dateUtils/dateUtils.js
+++ js/dateUtils/dateUtils.js
@@ -1,4 +1,4 @@
-// Import only the necessary functions from 'holidays.js'
+// businessDayUtils.js
 import { isHoliday } from '../api/holidays.js';
 
 // Function to format a date as a readable string
@@ -11,9 +11,10 @@ export function formatDate(date) {
 export function calculateBusinessDays(startDate, numDays, holidays) {
     let currentDate = new Date(startDate);
     let daysAdded = 0;
-    const past5pmCheckbox = document.getElementById('cbx-42');
-    // Adjust the end date based on the checkbox state
-    if (past5pmCheckbox.checked) {
+    const past5pmCheckbox = document.getElementById('cbx-42')?.checked; // Ensure the checkbox state is accessed correctly
+
+    // Adjust the start date based on the checkbox state
+    if (past5pmCheckbox) {
         currentDate.setDate(currentDate.getDate() + 1);
     }
 
@@ -27,4 +28,4 @@ export function calculateBusinessDays(startDate, numDays, holidays) {
     }
 
     return currentDate;
-} 
+}
