a6523b95fb9ccfcebd9d8661b334127fdee77596
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-09 00:27:25 +0800

    revised logic

--- js/ui/dateCalculation.js
+++ js/ui/dateCalculation.js
@@ -6,7 +6,7 @@ import { getHolidaysForCountry } from './countryUtils.js';
 function calculateIndianBusinessDays(startDate, numDays, holidays) {
     let currentDate = new Date(startDate);
     let businessDaysCount = 0;
-    const past5pmCheckbox = document.getElementById('cbx-42')?.checked; // Ensure the checkbox state is accessed correctly
+    const past5pmCheckbox = document.getElementById('cbx-42')?.checked;
 
     // Adjust the start date based on the checkbox state
     if (past5pmCheckbox) {
