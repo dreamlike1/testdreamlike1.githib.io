4bb21611ab73f4d74db26d5ddf5632b04269b0de
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-14 07:33:03 +0800

    delete past5pm

--- js/dateUtils/dateUtils.js
+++ js/dateUtils/dateUtils.js
@@ -6,26 +6,3 @@ export function formatDate(date) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return date.toLocaleDateString(undefined, options);
 }
-
-// Function to calculate the end date after adding a number of business days
-export function calculateBusinessDays(startDate, numDays, holidays) {
-    let currentDate = new Date(startDate);
-    let daysAdded = 0;
-    const past5pmCheckbox = document.getElementById('cbx-42')?.checked; // Ensure the checkbox state is accessed correctly
-
-    // Adjust the start date based on the checkbox state
-    if (past5pmCheckbox) {
-        currentDate.setDate(currentDate.getDate() + 1);
-    }
-
-    while (daysAdded < numDays) {
-        currentDate.setDate(currentDate.getDate() + 1);
-
-        // Check if the current date is a non-business day
-        if (!isNonBusinessDay(currentDate, holidays)) {
-            daysAdded++;
-        }
-    }
-
-    return currentDate;
-}
