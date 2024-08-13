552e538e3f1f40661c4430413803240210af35ee
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-14 06:48:32 +0800

    Add date selector module with Fomantic UI Calendar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+// main.js
 
-import { populateCountries } from './ui/countryUtils.js'; // Import country population and holiday fetching functions
-import { setupEventListeners } from './eventHandlers/eventHandlers.js'; // Import the event handlers
-import { setupSwitchButton } from './switch/switch.js'; // Import the switch button setup function
-import { initializeTimezone } from './timezone/timezone.js'; // Import the timezone initialization function
+import { populateCountries } from './ui/countryUtils.js';
+import { setupEventListeners } from './eventHandlers/eventHandlers.js';
+import { setupSwitchButton } from './switch/switch.js';
+import { initializeTimezone } from './timezone/timezone.js';
+import { initializeDateSelector } from './calendar/calendar.js'; // Import the date selector module
 
 document.addEventListener('DOMContentLoaded', () => {
     const defaultServiceType = 'expressPaid'; // Default service type
@@ -18,4 +20,5 @@ document.addEventListener('DOMContentLoaded', () => {
     setupEventListeners(); // Setup event listeners for UI elements
     setupSwitchButton(); // Initialize the switch button functionality
     initializeTimezone(); // Initialize the timezone functionality
+    initializeDateSelector(); // Initialize the date selector functionality
 });
