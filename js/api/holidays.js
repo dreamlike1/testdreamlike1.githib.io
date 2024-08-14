db10ad200a1cee9a13606781b7c2d46e7b27fc00
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-14 13:48:13 +0800

    change API

--- js/api/holidays.js
+++ js/api/holidays.js
@@ -7,7 +7,7 @@ const holidayCache = new Map();
 const noHolidayCountriesFromNager = [];
 
 // Fetch API key from environment variables (client-side)
-const CALENDERIFIC_API_KEY = '1YKZkmwxxc1oXCzeRoSrGl06PzgQxQtk';
+const CALENDERIFIC_API_KEY = 'EMIgkIkPLekUkjdA3ZhxFxFg7fM7E1qi';
 
 if (!CALENDERIFIC_API_KEY) {
   console.error('Calenderific API key is not defined in environment variables.');
