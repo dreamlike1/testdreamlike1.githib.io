b329b38103f38ff25832273636b873a679a50c60
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-15 15:39:29 +0800

    this is correct

--- js/ui/countryUtils.js
+++ js/ui/countryUtils.js
@@ -1,4 +1,4 @@
-/** js/ui/countryUtils.js
+//js/ui/countryUtils.js
 import { countryOptions, countryCodeMapping } from '../api/countryData.js';
 import { fetchHolidays } from '../api/holidays.js';
 
@@ -8,7 +8,6 @@ let holidaysCache = {};  // Cache to store holidays data for all countries
  * Populate the country dropdown with options based on the selected service type.
  * @param {string} serviceType - The service type to filter countries.
  */
-/**
 export async function populateCountries(serviceType = 'expressPaid') {
     const countrySelectDropdown = $('#countrySelect');
     const countries = countryOptions[serviceType] || [];
@@ -41,7 +40,6 @@ export async function populateCountries(serviceType = 'expressPaid') {
  * @param {jQuery} dropdown - The jQuery object of the dropdown element.
  * @param {Array} options - Array of options to populate the dropdown.
  */
-/**
 async function initializeDropdown(dropdown, options) {
     // Clear existing options
     dropdown.empty();
@@ -79,7 +77,6 @@ async function initializeDropdown(dropdown, options) {
  * Fetch holidays for each country and cache the results.
  * @param {Array} countries - Array of country names to fetch holidays for.
  */
-/**
 async function fetchAndCacheHolidays(countries) {
     console.log('Fetching holidays for countries:', countries);
     // Create an array of promises to fetch holidays for all countries
@@ -112,7 +109,6 @@ async function fetchAndCacheHolidays(countries) {
  * @param {string} country - The country for which to retrieve holidays.
  * @returns {Array} - An array of holiday objects for the specified country.
  */
-/**
 export function getHolidaysForCountry(country) {
     const countryCode = countryCodeMapping[country];
     if (!countryCode) {
@@ -123,4 +119,3 @@ export function getHolidaysForCountry(country) {
     }
     return holidaysCache[countryCode] || [];
 }
-**/
