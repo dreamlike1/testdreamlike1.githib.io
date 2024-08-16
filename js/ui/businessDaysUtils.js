a357815fb47564caf97ca373223d31951e84dcc9
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-16 15:36:05 +0800

    blank

--- js/ui/businessDaysUtils.js
+++ js/ui/businessDaysUtils.js
@@ -4,7 +4,7 @@ export function getBusinessDays(serviceType, country) {
         expressFree: '2-3',
         standard: {
             'New Zealand': '7-10',
-            'United States': ['5-8', '6-7'],
+            'United States': ' ',
             default: '5-8'
         },
         economy: {
