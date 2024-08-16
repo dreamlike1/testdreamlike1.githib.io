8aa079a29564c28b275552f4d70302b3b261315d
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-16 11:09:02 +0800

    revert

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-import { populateCountries, getHolidaysForCountry } from './ui/countryUtils.js';
+// main.js
+
+import { populateCountries } from './ui/countryUtils.js';
 import { setupEventListeners } from './eventHandlers/eventHandlers.js';
 import { setupSwitchButton } from './switch/switch.js';
 import { initializeTimezone } from './timezone/timezone.js';
-import { initializeDateSelector, updateCalendarWithHolidays } from './calendar/calendar.js'; // Import the date selector and update functions
+import { initializeDateSelector } from './calendar/calendar.js'; // Import the date selector module
 
 document.addEventListener('DOMContentLoaded', () => {
     const defaultServiceType = 'expressPaid'; // Default service type
@@ -19,14 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
     setupSwitchButton(); // Initialize the switch button functionality
     initializeTimezone(); // Initialize the timezone functionality
     initializeDateSelector(); // Initialize the date selector functionality
-
-    // Event listener for country selection
-    document.getElementById('countrySelect').addEventListener('change', function() {
-        const countryName = this.value;
-        // Retrieve holidays from cache for the selected country
-        const holidays = getHolidaysForCountry(countryName);
-        
-        // Update calendar with holidays
-        updateCalendarWithHolidays(holidays);
-    });
 });
