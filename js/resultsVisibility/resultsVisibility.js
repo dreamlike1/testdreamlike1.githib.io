538d9bb2bb23748558da9a19c37fff0c9d27b9ae
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-16 13:57:45 +0800

    visibility

--- js/resultsVisibility/resultsVisibility.js
+++ js/resultsVisibility/resultsVisibility.js
@@ -9,9 +9,22 @@ function handleVisibilityChange() {
     const serviceType = $('#serviceType').val();
     const country = $('#countrySelect').val();
 
-    if (serviceType === 'standard' && country === 'United States') {
+    let resultText = '';
+
+    // Adjust conditions and result text as needed
+    if (serviceType === 'standard') {
+        if (country === 'United States') {
+            resultText = 'Result for 5-8 Business Days';
+        } else {
+            resultText = 'Result for 6-7 Business Days';
+        }
         $('#standardResultField').removeClass('hidden');
+        $('#resultText').removeClass('hidden');
     } else {
         $('#standardResultField').addClass('hidden');
+        $('#resultText').addClass('hidden');
     }
+
+    // Set the text content based on the resultText variable
+    $('#resultTextMessage').text(resultText);
 }
