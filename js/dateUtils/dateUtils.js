dd57c8926f5fda8e9a5f516d93af772e8aaebe2c
chrysanthsy <81065268+dreamlike1@users.noreply.github.com>
2024-08-22 22:34:17 +0800

    Delete js directory

--- js/dateUtils/dateUtils.js
+++
@@ -1,8 +0,0 @@
-// businessDayUtils.js
-import { isHoliday } from '../api/holidays.js';
-
-// Function to format a date as a readable string
-export function formatDate(date) {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString(undefined, options);
-}
